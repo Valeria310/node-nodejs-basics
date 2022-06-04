7b2dbbb0dee674522d315e7e8c968ba227f54bef
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-04 20:53:59 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,15 @@
+import fs from 'fs'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+import path from 'path';
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
 export const read = async () => {
-    // Write your code here 
+    fs.readFile(path.join(__dirname, 'files', 'fileToRead.txt'), 'utf-8', (err, data) => {
+        if(err) throw new Error('FS operation failed')
+        console.log(data)
+    })
 };
+read()
