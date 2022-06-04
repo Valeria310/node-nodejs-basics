cb52db847835e99fee8d717d804060f1f3b4d49f
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-04 20:52:57 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,22 @@
+import fs from 'fs'
+import path from 'path'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
 export const create = async () => {
-    // Write your code here 
+    fs.access(path.join(__dirname, 'files', 'fresh.txt'), fs.F_OK, (err) => {
+        if (err) {
+            fs.writeFile(path.join(__dirname, 'files', 'fresh.txt'), 'I am fresh and young', err=>{
+                    if(err) {
+                        console.error(err)
+                    }
+                })  
+        } else {
+            throw new Error('FS operation failed');
+        }
+    })
 };
+create();
