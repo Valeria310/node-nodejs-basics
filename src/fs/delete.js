7f291888a2575be7af3f30c1cc56acd3ca70135e
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-04 20:53:40 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,13 @@
+import fs from 'fs'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+import path from 'path';
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
 export const remove = async () => {
-    // Write your code here 
+    fs.unlink(path.join(__dirname, 'files', 'fileToRemove.txt'), err=>{
+        if(err) throw new Error('FS operation failed')
+    })
 };
+remove()
