22840da9896e8e9895a886aa187c490c006d72fe
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-04 20:53:27 +0300

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,17 @@
+import fs from 'fs'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+import path from 'path';
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
 export const rename = async () => {
-    // Write your code here 
+    fs.access(path.join(__dirname, 'files', 'properFilename.md'), err=>{
+        if(!err) throw new Error('FS operation failed') 
+    })
+    fs.rename(path.join(__dirname, 'files', 'wrongFilename.txt'), path.join(__dirname, 'files', 'properFilename.md'), err =>{
+        if(err) throw new Error('FS operation failed')
+    })
 };
+rename()
