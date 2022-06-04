e06e4685ee16e9be5fcaaf276f7a40ca448831b6
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-04 20:53:07 +0300

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,30 @@
+import fs from 'fs'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+import path from 'path';
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
+
 export const copy = async () => {
-    // Write your code here 
+    fs.access(path.join(__dirname, 'files'), fs.F_OK, (err)=>{
+        throw new Error('FS operation failed')
+    })
+    fs.readdir(path.join(__dirname, 'files'), (err, data)=>{
+        if(err) {
+            throw new Error('FS operation failed')
+        }
+        fs.mkdir(path.join(__dirname, 'files_copy'), err=>{
+            if(err) {
+                throw new Error('FS operation failed')
+            }
+            data.forEach(el => {
+                copyFile(path.join(__dirname, 'files2', el), path.join(__dirname, 'files_copy', el), err=>{
+                    if(err) throw err
+                })
+            })
+        })
+    })
 };
+copy();
