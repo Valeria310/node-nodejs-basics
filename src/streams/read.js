fde92cc5c330f245543131964064e18082f54c74
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 16:18:24 +0300

    feat: implement read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,13 @@
+import fs from 'fs'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+import path from 'path';
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
 export const read = async () => {
-    // Write your code here 
+    const readebleStream = fs.createReadStream(path.join(__dirname,'files','fileToRead.txt'));
+    readebleStream.on('data', chunk => process.stdout.write(chunk));
 };
+read()
