003d76a6155f7992ed610ef3e0cc999810fd8a3a
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 14:57:11 +0300

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,13 @@
+import crypto from 'crypto'
+import path from 'path';
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash("sha256", path.parse(__dirname, 'files', 'fileToCalculateHashFor.txt'))
+    console.log(hash.digest('hex'))
 };
+calculateHash()
