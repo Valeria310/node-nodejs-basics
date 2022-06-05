1dd53e7ac52652c46f87816fddcc7c8e1726f0a5
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 21:32:55 +0300

    feat: implement compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,17 @@
+import fs from 'fs'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+import path from 'path';
+import zlib from 'zlib';
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
 export const compress = async () => {
-    // Write your code here 
+    const read = fs.createReadStream(path.join(__dirname,'files','fileToCompress.txt'), 'utf-8');
+    const write = fs.createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+    const gzip = zlib.createGzip();
+    read.pipe(gzip).pipe(write);
 };
+compress()
+
