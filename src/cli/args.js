6cf812a8b1e3c3a3c44374e43a60c75bf2f36e45
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 13:37:01 +0300

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,9 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    for (let i=0; i<args.length; i++) {
+        if(args[i].slice(0,2)=='--' && args[i+1]) {
+            console.log(args[i], 'is', args[i+1])
+        }
+    }
 };
+parseArgs()
