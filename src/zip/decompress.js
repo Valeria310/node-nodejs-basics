bf8d9a53cd07d53b496085d881e2ae42b12ea3ca
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 21:41:40 +0300

    feat: implement decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,16 @@
+import fs from 'fs'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+import path from 'path';
+import zlib from 'zlib';
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
 export const decompress = async () => {
-    // Write your code here 
+    const read = fs.createReadStream(path.join(__dirname, 'files', 'archive.gz'));
+    const write = fs.createWriteStream(path.join(__dirname,'files','fileToCompress2.txt'));
+    const gzip = zlib.createGunzip();
+    read.pipe(gzip).pipe(write);
 };
+decompress()
