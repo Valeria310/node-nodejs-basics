14ab2b4b9f8bb696ca579088a76372232f90c5a9
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 20:52:56 +0300

    feat: implement transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,16 @@
+import {Transform} from 'stream'
+
 export const transform = async () => {
-    // Write your code here 
+    const revert = new Transform({
+        transform(chunk, encoding, callback) {
+            callback()
+        }
+    })
+    process.stdin.on('data', function(data) {
+        revert._transform(data, 'utf-8', ()=>{
+            data = data.reverse()
+        });
+        process.stdout.write(data)
+    })
 };
+transform()
