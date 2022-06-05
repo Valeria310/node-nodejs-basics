c8fe60cc7d704773192f2c23619bb1e8ef24cc55
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 20:14:32 +0300

    feat: implement write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import fs from 'fs'
+import commonjsVariables from 'commonjs-variables-for-esmodules'
+import path from 'path';
+
+const {
+    __dirname
+} = commonjsVariables(import.meta)
+
 export const write = async () => {
-    // Write your code here 
+    const writebleStream = fs.createWriteStream(path.join(__dirname,'files','fileToWrite.txt'));
+    process.stdin.on('data', function(data) {
+        writebleStream.write(data)
+    })
 };
+write()
