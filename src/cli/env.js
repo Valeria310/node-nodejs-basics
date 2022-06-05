39d729426697e4743b9ba222c2d40ae6b88aae92
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 08:01:41 +0300

    feat: implement env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const envs = process.env;
+    for (let key in envs) {
+        if(key.slice(0,4)==='RSS_') {
+            console.log(key+'='+envs[key])
+        }
+    }
 };
+parseEnv()
