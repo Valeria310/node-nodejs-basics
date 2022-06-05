e6893c883399b0b4f1b769665aa5a755f46e47e1
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-05 23:20:58 +0300

    feat: implement main and worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,23 @@
+import { resolve } from 'dns/promises';
+import {cpus} from 'os'
+import {Worker} from 'worker_threads'
+
 export const performCalculations = async () => {
-    // Write your code here
+    const datas=[];
+    for (let i=0; i<cpus().length; i++) {
+        datas.push(i+10)
+    }
+    const result = (data)=>{
+        return new Promise((res,rej)=>{
+            const worker = new Worker('./worker.js', {
+                workerData: data
+            })
+            worker.on('message', msg=>{
+                res(msg)
+            })
+        })
+    }
+    const results = await Promise.all(datas.map(data => result(data)));
+    console.log(results)
 };
+performCalculations()
